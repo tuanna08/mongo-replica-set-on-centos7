b31d84705497beeb6653d8112f37662f5b4785c6
nguyen anh tuan <tuanna08go@gmail.com>
2020-05-06 16:51:00 +0700

    update

--- setup-hosts.sh
+++ setup-hosts.sh
@@ -3,6 +3,6 @@
 echo "Create by Openstack!"
 
 
-echo "10.19.3.2  mg-node01" >> /etc/hosts
-echo "10.19.3.3  mg-node02" >> /etc/hosts
-echo "10.19.3.4  mg-node03" >> /etc/hosts
+echo "10.19.3.2  node01" >> /etc/hosts
+echo "10.19.3.3  node02" >> /etc/hosts
+echo "10.19.3.4  node03" >> /etc/hosts
